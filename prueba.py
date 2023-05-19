534487796584f021ccfd6e6349c8ae931bd76157
Santiago Pourteau <spourteau@udesa.edu.ar>
2023-05-18 22:09:51 -0300

    Consigue Toda la Informacion Correcta

--- prueba.py
+++ prueba.py
@@ -16,9 +16,8 @@ def get_info(url: str, headers, parser: str):
 
     Age = pageSoup.find_all("td", {"class": "zentriert"})
 
-    Positions = pageSoup.find_all("td", {"class": ["zentriert rueckennummer bg_Torwart", "zentriert rueckennummer bg_Abwehr", "zentriert rueckennummer bg_Mittelfeld", "zentriert rueckennummer bg_Sturm"]})
-    if(len(Positions)==0):
-        print("poo")
+    Positions = pageSoup.find_all("table", {"class": "inline-table"})
+    
     Nationality = pageSoup.find_all("td", {"class": "zentriert"})
 
     for i in range(0,len(Players)):
@@ -28,30 +27,40 @@ def get_info(url: str, headers, parser: str):
         AgeList.append(str(Age[i]).split('">')[1].split("<",1)[0])
 
     for i in range(0,len(Positions)): #no funciona
-        print(str(Positions[i]).split('title="',1)[1].split('"><div')[0])
-        if(len(str(Positions[i]).split('title="',1)[1].split('"><div')[0]) == 0):
-            print("hi")
-        PositionsList.append(str(Positions[i]).split('title="',1)[1].split('"><div')[0])
+        Positions[i] = str(Positions[i]).split('<td>',1)[1].split('</td>')[0]
+        try:
+            Positions[i] = str(Positions[i]).split('</a>',1)[1]
+            if(1 < len(Positions[i]) < 50):
+                PositionsList.append(Positions[i])
+        except:
+            if(1 < len(Positions[i]) < 50):
+                PositionsList.append(Positions[i])
+        else:
+            if(1 < len(Positions[i]) < 50):
+                PositionsList.append(Positions[i])
+
+
 
     for i in range(2,(len(Players)*3),3):
         NationList.append(str(Nationality[i]).split('title="',1)[1].split('"/',1)[0])
-    return (PlayersList,AgeList,NationList)
+    return (PlayersList,AgeList,NationList,PositionsList)
 
 headers = {'User-Agent' : 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/113.0.0.0 Safari/537.36'}
 boca = get_info("https://www.transfermarkt.com/club-atletico-boca-juniors/alumni/verein/189", headers, 'html.parser')
-boca_jug, boca_edad, boca_pais = boca[0], boca[1], boca[2]
+boca_jug, boca_edad, boca_pais, boca_pos = boca[0], boca[1], boca[2], boca[3]
 
 river = get_info("https://www.transfermarkt.com/club-atletico-river-plate/alumni/verein/209", headers, 'html.parser')
-river_jug, river_edad, river_pais = river[0], river[1], river[2]
+river_jug, river_edad, river_pais, river_pos = river[0], river[1], river[2], river[3]
 
 slo = get_info("https://www.transfermarkt.com/club-atletico-san-lorenzo-de-almagro/alumni/verein/1775", headers, 'html.parser')
-slo_jug, slo_edad, slo_pais = slo[0], slo[1], slo[2]
+slo_jug, slo_edad, slo_pais, slo_pos = slo[0], slo[1], slo[2], slo[3]
 
 todo_jug = boca_jug + river_jug + slo_jug
+todo_pos = boca_pos + river_pos + slo_pos
 todo_edad = boca_edad + river_edad + slo_edad
 todo_pais = boca_pais + river_pais + slo_pais
 
 
-final_df = pd.DataFrame({"Player": todo_jug, "Age": todo_edad, "Nation": todo_pais})
+final_df = pd.DataFrame({"Player": todo_jug, "Position": todo_pos,"Age": todo_edad, "Nation": todo_pais})
 final_df.to_excel('/Users/Colegio/Desktop/TaTeTi-Futbol/player_data.xlsx',index=False)
 
