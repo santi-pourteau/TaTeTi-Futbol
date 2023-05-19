b5cabf34c6168357fdf2f0dfac4929340497afb4
Santiago Pourteau <spourteau@udesa.edu.ar>
2023-05-18 23:02:08 -0300

    Conseguir info funciona (lento)

--- base_de_datos.py
+++ base_de_datos.py
@@ -1,9 +1,10 @@
 import requests 
 from bs4 import BeautifulSoup
 import pandas as pd
-import sys
 
-def get_info(url: str, headers, parser: str):
+headers = {'User-Agent' : 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/113.0.0.0 Safari/537.36'}
+
+def get_info_page(url: str, headers, parser: str):
     page1 = url
     pageTree = requests.get(page1,headers=headers)
     pageSoup = BeautifulSoup(pageTree.content, 'html.parser')
@@ -44,23 +45,39 @@ def get_info(url: str, headers, parser: str):
     for i in range(2,(len(Players)*3),3):
         NationList.append(str(Nationality[i]).split('title="',1)[1].split('"/',1)[0])
     return (PlayersList,AgeList,NationList,PositionsList)
+def get_info_team(url, headers):
+    jugadores = []
+    edades = []
+    paises = []
+    posiciones = []
+    i = 1
+    jugadores_prev = ['a']
+    while(True): # <= 41
+        print(i)
 
-headers = {'User-Agent' : 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/113.0.0.0 Safari/537.36'}
-boca = get_info("https://www.transfermarkt.com/club-atletico-boca-juniors/alumni/verein/189", headers, 'html.parser')
-boca_jug, boca_edad, boca_pais, boca_pos = boca[0], boca[1], boca[2], boca[3]
-
-river = get_info("https://www.transfermarkt.com/club-atletico-river-plate/alumni/verein/209", headers, 'html.parser')
-river_jug, river_edad, river_pais, river_pos = river[0], river[1], river[2], river[3]
-
-slo = get_info("https://www.transfermarkt.com/club-atletico-san-lorenzo-de-almagro/alumni/verein/1775", headers, 'html.parser')
-slo_jug, slo_edad, slo_pais, slo_pos = slo[0], slo[1], slo[2], slo[3]
-
-todo_jug = boca_jug + river_jug + slo_jug
-todo_pos = boca_pos + river_pos + slo_pos
-todo_edad = boca_edad + river_edad + slo_edad
-todo_pais = boca_pais + river_pais + slo_pais
+        equipo = get_info_page(f"{url}/page/{i}", headers, 'html.parser')
+        if(jugadores_prev[0] == equipo[0][0]):
+            break
+        jugadores_prev = equipo[0]
+        jugadores += equipo[0]
+        edades += equipo[1]
+        paises += equipo[2]
+        posiciones += equipo[3]
+        i+=1
+    print(i)
+    return (jugadores,edades,paises,posiciones)
 
 
-final_df = pd.DataFrame({"Player": todo_jug, "Position": todo_pos,"Age": todo_edad, "Nation": todo_pais})
+
+boca = get_info_team("https://www.transfermarkt.com/ca-boca-juniors/alumni/verein/189",headers)
+river = get_info_team("https://www.transfermarkt.com/club-atletico-river-plate/alumni/verein/209", headers, 'html.parser')
+slo = get_info_team("https://www.transfermarkt.com/club-atletico-san-lorenzo-de-almagro/alumni/verein/1775", headers, 'html.parser')
+
+todo_jug = boca[0] + river[0] + slo[0]
+todo_pos = boca[1] + river[1] + slo[1]
+todo_edad = boca[2] + river[2] + slo[2]
+todo_pais = boca[3] + river[3] + slo[3]
+
+final_df = pd.DataFrame({"Player": todo_jug, "Position": todo_pos[1],"Age": todo_edad[2], "Nation": todo_pais})
 final_df.to_excel('/Users/Colegio/Desktop/TaTeTi-Futbol/player_data.xlsx',index=False)
 
